184b66a2d10ba87ae2604c275f46c981ebf158ba
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-10-07 18:20:20 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡Gym leagua", HidePremium = false, IntroText = "Script for Gym leagua", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
+local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡Pet Simulator X", HidePremium = false, IntroText = "Script for Gym leagua", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
 
 local Humanoid = game.Players.LocalPlayer.Character.Humanoid
 local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
@@ -24,6 +24,17 @@ function AutoFarmDiamonds()
 	end
 end
 
+function AutoOpenEggs()
+	while _G.AutoOpenEggs == true do
+		if HumanoidRootPart.CFrame ~= CFrame.new(6971.98975, -155.867203, 4101.33984, -0.0930883139, 1.1291386e-07, -0.995657861, 5.01153075e-08, 1, 1.08720798e-07, 0.995657861, -3.9777067e-08, -0.0930883139) then
+			HumanoidRootPart.CFrame = CFrame.new(6971.98975, -155.867203, 4101.33984, -0.0930883139, 1.1291386e-07, -0.995657861, 5.01153075e-08, 1, 1.08720798e-07, 0.995657861, -3.9777067e-08, -0.0930883139)
+		end
+		wait(0.5)
+		game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(BarnDoodleEgg))
+		wait(3.3)
+	end
+end
+
 function WalkSpeed()
 	while _G.WalkSpeed do game:GetService("RunService").RenderStepped:wait()
 	    Humanoid.WalkSpeed = _G.WalkSpeed
@@ -36,6 +47,12 @@ function JumpPower()
     end
 end
 
+local BarnDoodleEgg = {
+    [1] = "Barn Doodle Egg",
+    [2] = false,
+    [3] = false
+}
+
 local Tab = Window:MakeTab({
 	Name = "AutoFarm",
 	Icon = "rbxassetid://4483362748",
@@ -43,11 +60,28 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddToggle({
-	Name = "AutoFarm Diamonds",
+	Name = "Auto Farm Diamonds",
 	Default = false,
 	Callback = function(Value)
 		_G.AutoFarmDiamonds = Value
 		AutoFarmDiamonds()
+	end    
+})
+
+Tab:AddToggle({
+	Name = "Auto Farm Fruits",
+	Default = false,
+	Callback = function(Value)
+		
+	end    
+})
+
+Tab:AddToggle({
+	Name = "Auto Open Eggs",
+	Default = false,
+	Callback = function(Value)
+		_G.AutoOpenEggs = Value
+		AutoOpenEggs()
 	end    
 })
 
