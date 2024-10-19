ccc03bf428a8da223cfe2fd2d00d33ce884114f2
Swordikk <133685730+Swordikk@users.noreply.github.com>
2024-10-19 19:06:13 +0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,39 +1,9 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡Gym leagua", HidePremium = false, IntroText = "Script for Gym leagua", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
+local Window = OrionLib:MakeWindow({Name = "Script by Swordikk | ⚡Realistik_Driving_Sinulator", HidePremium = false, IntroText = "Script for Malfoy4ik", SaveConfig = true, IntroEnabled = true, ConfigFolder = "Scripts"})
 
 local Humanoid = game.Players.LocalPlayer.Character.Humanoid
 local HumanoidRootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
-
-function AutoFarmDiamonds()
-	while _G.AutoFarmDiamonds == true do
-		for i, v in pairs(game:GetService("Workspace"):WaitForChild("__THINGS").Coins:GetChildren()) do
-            if (HumanoidRootPart.Position - v.Coin.Position).magnitude < 200 and (
-				v.Coin.MeshId == "rbxassetid://13087914804" or 
-				v.Coin.MeshId == "rbxassetid://13087826929" or 
-				v.Coin.MeshId == "rbxassetid://13087884875"or 
-				v.Coin.MeshId == "rbxassetid://6356575794") then wait(0.5)
-                HumanoidRootPart.CFrame = v.Coin.CFrame
-			else
-				for i, v in pairs(game:GetService("Workspace"):WaitForChild("__THINGS").Lootbags:GetChildren()) do
-					wait(0.5)
-					HumanoidRootPart.CFrame = v.CFrame
-				end
-            end
-		end
-	end
-end
-
-function AutoOpenEggs()
-	while _G.AutoOpenEggs == true do
-		if HumanoidRootPart.CFrame ~= CFrame.new(6971.98975, -155.867203, 4101.33984, -0.0930883139, 1.1291386e-07, -0.995657861, 5.01153075e-08, 1, 1.08720798e-07, 0.995657861, -3.9777067e-08, -0.0930883139) then
-			HumanoidRootPart.CFrame = CFrame.new(6971.98975, -155.867203, 4101.33984, -0.0930883139, 1.1291386e-07, -0.995657861, 5.01153075e-08, 1, 1.08720798e-07, 0.995657861, -3.9777067e-08, -0.0930883139)
-		end
-		wait(0.5)
-		game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(BarnDoodleEgg))
-		wait(3.3)
-	end
-end
 
 function WalkSpeed()
 	while _G.WalkSpeed do game:GetService("RunService").RenderStepped:wait()
@@ -54,26 +24,10 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddToggle({
-	Name = "Auto Farm Diamonds",
-	Default = false,
-	Callback = function(Value)
-		_G.AutoFarmDiamonds = Value
-		AutoFarmDiamonds()
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Farm Fruits",
+	Name = "AutoFarm Miles",
 	Default = false,
 	Callback = function(Value)
 		
-	end    
-})
-
-Tab:AddToggle({
-	Name = "Auto Claim Gifts",
-	Default = false,
-	Callback = function(Value)
 		
 	end    
 })
